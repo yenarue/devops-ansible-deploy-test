1792012dda16fc29b3d92217759c260b55b80e94
yenarue <yenarue@gmail.com>
2018-05-22 23:54:43 +0900

    chore: add build config for CircleCI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,15 +17,19 @@ end
 
 host = ENV['TARGET_HOST']
 
-`vagrant up #{host}`
+if ENV['CIRCLECI']
+  options = Net::SSH::Config.for(host, ["~/.ssh/config"])
+  options[:user] = "root"
+else
+  `vagrant up #{host}`
 
-config = Tempfile.new('', Dir.tmpdir)
-config.write(`vagrant ssh-config #{host}`)
-config.close
+  config = Tempfile.new('', Dir.tmpdir)
+  config.write(`vagrant ssh-config #{host}`)
+  config.close
 
-options = Net::SSH::Config.for(host, [config.path])
-
-options[:user] ||= Etc.getlogin
+  options = Net::SSH::Config.for(host, [config.path])
+  options[:user] ||= Etc.getlogin
+end
 
 set :host,        options[:host_name] || host
 set :ssh_options, options
