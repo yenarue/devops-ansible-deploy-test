ffbeebf4f9a2eea5f34c9953552ea7662404f7fe
yenarue <yenarue@gmail.com>
2018-05-24 02:10:02 +0900

    chore: add build script for logapp

--- digitalocean.tf
+++ digitalocean.tf
@@ -11,3 +11,15 @@ resource "digitalocean_droplet" "webapp" {
 		command = "echo ${digitalocean_droplet.webapp.ipv4_address} ${digitalocean_droplet.webapp.name} >> /tmp/hosts.txt"
 	}
 }
+
+resource "digitalocean_droplet" "logapp" {
+	image = "cenos-7-x64"
+	name = "logapp"
+	region = "sgp1"
+	size = "512mb"
+	ssh_keys = [ "${var.ssh_keys_id}" ]
+
+	provisioner "local-exec" {
+		command = "echo ${digitalocean_droplet.logapp.ipv4_address} ${digitalocean_droplet.logapp.name} >> /tmp/hosts.txt"
+	}
+}
